b2de752366427912dab579e67287ebdd98ee2862
SuRang_ <35811799+kstu5474@users.noreply.github.com>
2025-05-20 01:29:53 +0900

    update

--- src/main/java/io/github/surang_volkov/minecivilization/commands/Guild.java
+++ src/main/java/io/github/surang_volkov/minecivilization/commands/Guild.java
@@ -1,12 +1,11 @@
 package io.github.surang_volkov.minecivilization.commands;
 
 import io.github.surang_volkov.minecivilization.tools.ChunkManager;
+import io.github.surang_volkov.minecivilization.tools.GuildManager;
 import io.github.surang_volkov.minecivilization.tools.SubCommand;
 import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
-import java.util.Map;
 
 import static io.github.surang_volkov.minecivilization.MineCivilization.infoLog;
 
@@ -14,19 +13,76 @@ public class Guild implements SubCommand {
 
     @Override
     public void execute(CommandSender commandSender, String[] args) {
+        Player p = (Player) commandSender;
+        int index = ChunkManager.getChunkIndex(p.getChunk().getX(),p.getChunk().getZ());
         if (args.length == 0) {
-            commandSender.sendMessage("§e사용법: /mcv chunk <edit|setorigin>");
+            commandSender.sendMessage("§e사용법: /mcv guild <invite|leave|kick|status|create|mandate|promote|demote|delete>");
         }
         switch (args[0].toLowerCase()){
-            case "edit":
-                commandSender.sendMessage("§e사용법: /mcv chunk edit <index> <attributes> <arguments>");
+            case "invite":
                 break;
-            case "setorigin":
+            case "leave":
+                break;
+            case "kick":
+                break;
+            case "status":
+                break;
+
+            case "create":
                 if (args.length == 1) {
-                    commandSender.sendMessage("§e사용법: /mcv chunk setorigin <limit>");
+                    commandSender.sendMessage("§e사용법: /mcv guild create <(띄어쓰기 없이)길드이름>");
                     return;
                 }
                 if (args.length == 2) {
+                    GuildManager.GenerateGuild(p,args[1],index);
+                    return;
+                }
+                break;
+
+            case "mandate":
+                if (args.length == 1) {
+                    commandSender.sendMessage("§e사용법: 다른 유저에게 길드장의 권한을 위임합니다. /mcv guild mandate <유저이름>");
+                    return;
+                }
+                if (args.length == 2) {
+                    commandSender.sendMessage("§e정말로 위임하시겠습니까?\n동의하시면 /mcv guild mandate <유저이름> confirm 을 입력해주세요.");
+                    return;
+                }
+                if (args.length == 3) {
+                    return;
+                }
+                break;
+
+            case "promote":
+                if (args.length == 1) {
+                    commandSender.sendMessage("§e사용법: 부길드장을 임명합니다. /mcv guild promote <유저이름>");
+                    return;
+                }
+                if (args.length == 2) {
+                return;
+                }
+                break;
+
+            case "demote":
+                if (args.length == 1) {
+                    commandSender.sendMessage("§e사용법: 부길드장을 해임합니다. /mcv guild demote <유저이름>");
+                    return;
+                }
+                if (args.length == 2) {
+                    return;
+                }
+                break;
+
+            case "delete":
+                if (args.length == 1) {
+                    commandSender.sendMessage("§e사용법: /mcv guild delete <길드이름>");
+                    return;
+                }
+                if (args.length == 2) {
+                    commandSender.sendMessage("§e정말 길드를 해산하시겠습니까?\n§e동의하시면 /mcv guild delete <길드이름> confirm 을 입력해주세요.");
+                    return;
+                }
+                if (args.length == 3) {
                     return;
                 }
                 break;
--- src/main/java/io/github/surang_volkov/minecivilization/tools/GuildManager.java
+++ src/main/java/io/github/surang_volkov/minecivilization/tools/GuildManager.java
@@ -1,7 +1,69 @@
 package io.github.surang_volkov.minecivilization.tools;
 
-public class GuildManager {
-    private static class GuildProperties{
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 
+import javax.swing.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GuildManager {
+    public static void GenerateGuild(Player p, String name, int index){
+        FileConfiguration guildConfig = DataManager.getGuildsConfig();
+        Map<String , Map<String,Object>> GuildData = new HashMap<>();
+        GuildData.put(name,new GuildProperties.Builder()
+                .leader(p.getName())
+                .viceLeader("none")
+                .safeZone(List.of(Integer.toString(index)))
+                .build());
+    }
+    private static class GuildProperties{
+        public String name;
+        public String rank;
+        public String leader;
+        public String viceLeader;
+        public List<String> members; // default : {prodamp-1, tradeadvantage-1}
+        public List<String> claimed; // default : {iron-1, paper-1}
+        public List<String> safeZone;
+
+        public GuildProperties(Builder builder) {
+            this.name = builder.name;
+            this.rank = builder.rank;
+            this.leader = builder.leader;
+            this.viceLeader = builder.viceLeader;
+            this.members = builder.members;
+            this.claimed = builder.claimed;
+            this.safeZone = builder.safeZone;
+        }
+        public static class Builder {
+            private String name;
+            private String rank;
+            private String leader = "unclaimed"; // border, safezone, claimed, unclaimed
+            private String viceLeader = "none"; // default : none
+            private List<String> members;
+            private List<String> claimed;
+            private List<String> safeZone;
+
+            public Builder name(String name) {this.name = name;return this;}
+            public Builder rank(String rank) {this.rank = rank;return this;}
+            public Builder leader(String leader) {this.leader = leader;return this;}
+            public Builder viceLeader(String viceLeader) {this.viceLeader = viceLeader;return this;}
+            public Builder members(List<String> members) {this.members = members;return this;}
+            public Builder claimed(List<String> claimed) {this.claimed = claimed;return this;}
+            public Builder safeZone(List<String> safeZone) {this.safeZone = safeZone;return this;}
+
+            public Map<String, Object> build() {
+                Map<String, Object> map = new HashMap<>();
+                //map.put("name", name);
+                map.put("rank", rank);
+                map.put("leader", leader);
+                map.put("vice-leader", viceLeader);
+                map.put("members", members);
+                map.put("claimed", claimed);
+                map.put("safe-zone", safeZone);
+                return map;
+            }
+        }
     }
 }
